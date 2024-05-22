1da0848485c549ca983624a16718c7d8e4d08dd5
shyunku <whdudgns7321@gmail.com>
2024-05-22 15:36:59 +0900

    fix p-queue dependency error

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": ["warn", { allowExpressions: true }],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    indent: ["error"],
+  },
+  overrides: [
+    {
+      files: ["test/**/*"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
